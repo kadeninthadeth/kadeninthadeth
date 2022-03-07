65858581a8f70237626d63968ce18eaa28890b28
kadeninthadeth <99894079+kadeninthadeth@users.noreply.github.com>
2022-03-07 13:04:35 -0500

    Create perroquet.js

---
+++ perroquet.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <center> <title> Javascript </title> </center>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <center> <h1> Mon Javascript </h1> </center>
+    <center> <button onclick="money()"> Cliquez ici! </button> </center>
+    <script>
+      function money(){
+        alert("Woah Calme Toi");
+      }
+      
+      var poutine = prompt("Eh! veulent venir manger? Eh!");
+      alert( poutine + ", Okay parce que je suis faim. ");
+      
+      var love = prompt(" Do you love me? ");
+      alert( love + "! I Love you too <3 ");
+      
+      var age = prompt(" Quel âge avez-vous? ");
+      alert( age + "! OH OKAY COOL! Moi Aussi ");
+      
+      var couleur = prompt(" Quelle est votre couleur préférée?");
+      alert("Ah! Le mien est mauve ");
+    </script>
+  </body>
+</html>
