a592933ce9403aac28e6674e0ee9085102a0289a
kadeninthadeth <99894079+kadeninthadeth@users.noreply.github.com>
2022-04-21 14:52:05 -0400

    Update FORMULAIRE_AFFICHEUR_DE_NOM.js

--- FORMULAIRE_AFFICHEUR_DE_NOM.js
+++ FORMULAIRE_AFFICHEUR_DE_NOM.js
@@ -1,21 +1,34 @@
-function afficheInitiales() {
-  var prenom = document.getElementById("txtPrenom").value
-  var milieu = document.getElementById("txtMilieu").value
-  var nom = document.getElementById("txtNom").value
-  var afficheI = document.getElementById("divAfficheI")
-  afficheI.innerHTML = "<br>" + prenom.charAt(0) + "." + milieu.charAt(0) + "." + nom.charAt(0) + "."
+function confirmeQuiz() {
+  //affichage pour le nom
+var nom = document.getElementById("txtNom").value
+var afficheN = document.getElementById("divAfficheN");
+  if (nom == "") {
+    afficheN.innerHTML = "Erreur tu as oublie de mettre ton nom!"
+  } else {
+    afficheN.innerHTML = "Salut, " + nom
 }
-function afficheNom() {
-  var prenom = document.getElementById("txtPrenom").value
-  var milieu = document.getElementById("txtMilieu").value
-  var nom = document.getElementById("txtNom").value
-  var afficheN = document.getElementById("divAfficheN")
-  afficheN.innerHTML = "<br>" + prenom + " " + milieu + " " + nom
+  //affichage pour le select
+var selectVar = document.getElementById("selVar").value
+var afficheV = document.getElementById("divAfficheV");
+  if(selectVar == "3"){
+    afficheV.innerHTML = "1. " + selectVar + " est correct!"
+  } else {
+    afficheV.innerHTML = "1. Erreur dans la question."
+  }
+  //affichage pour le radiobox
+var radiosPrompt = document.getElementsByName("prompt");
+var afficheC = document.getElementById("divAfficheC");
+  if(radiosPrompt[2].checked) {
+    afficheC.innerHTML = "2. " + radiosPrompt[2].value + " est correct!"
+  } else {
+    afficheC.innerHTML = "2. Erreur dans la question."
 }
-function afficheUser() {
-  var prenom = document.getElementById("txtPrenom").value
-  var milieu = document.getElementById("txtMilieu").value
-  var nom = document.getElementById("txtNom").value
-  var afficheU = document.getElementById("divAfficheU")
-  afficheU.innerHTML = "<br>" + prenom.toLowerCase().charAt(0) + milieu.toLowerCase().charAt(0) + nom.toLowerCase()
+  //affichage pour le checkbox
+var checkBox = document.getElementsByName("chkQuiz");
+var afficheQ = document.getElementById("divAfficheQ");
+  if(checkBox[1].checked && checkBox[3].checked && !checkBox[0].checked && !checkBox[2].checked) {
+    afficheQ.innerHTML = "3. " + checkBox[1].value + " et " + checkBox[3].value + " est correct!"
+  } else {
+    afficheQ.innerHTML = "3. Erreur dans la question."
+  }
 }
